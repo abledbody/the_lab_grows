690f31015e29ba0feaffcfbc1007e283cca1f34e
Drew "abledbody" Mudge <abledbody.gamedev@gmail.com>
2025-03-27 22:42:00 -0400

    Devops (#2)
    * Fix game include path (again)
    
    * Extend basic game.lua layout
    
    * Fix lua runtime version
    
    * Add require and error explorer libraries

--- src/game.lua
+++ src/game.lua
@@ -1,7 +1,23 @@
+include"src/require.lua"
+
+-- Dependencies
+
+-- Constants
+DT = 1/60
+
+-- Game state
+
+-- Picotron hooks
+function _init()
+
+end
+
 function _update()
-    
+	
 end
 
 function _draw()
-    
+	
 end
+
+include"src/error_explorer.lua"
