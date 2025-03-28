1a98efdaa76ad906a463c6e52f6fa332e160d523
Drew abledbody Mudge <abledbody.gamedev@gmail.com>
2025-03-27 22:32:44 -0400

    Fix game include path (again)

--- the_lab_grows.p64/main.lua
+++ the_lab_grows.p64/main.lua
@@ -1,2 +1,8 @@
-include"projects/the_lab_grows/src/game.lua"
---include"game.lua"
+-- Development
+cd("/projects/the_lab_grows")
+DATP = "the_lab_grows.p64/"
+-- Distribution - Copy `src` folder into `the_lab_grows.p64`
+--DATP = ""
+
+-- ... --
+include"src/game.lua"
