ccfe63a5397d882a22fb1b9c50059afe504b5fc2
Drew "abledbody" Mudge <abledbody.gamedev@gmail.com>
2025-03-28 20:12:05 -0400

    Animation script (#3)
    - Added a new animation script.
      - Added `Animator` class.
      - Added `Animation` class.
    - `game.lua` now changes the palette on initiation and displays an idle animation using this animator.

---
+++ src/animation.lua
@@ -0,0 +1,111 @@
+--- @class Animation A table containing a timing array and an arbitrary number of
+--- other arrays from which to pull animation data. The indices of each array
+--- correspond to a particular frame. If any data is at frame index -1, it will
+--- be used for every frame of the animation.
+--- @field duration [number] An array indicating how long it takes for each frame to elapse. Does not support index -1.
+
+--- Advances the time of the animator by dt.
+--- @param self Animator The animator.
+--- @param dt number The time to advance by.
+local function advance(self,dt)
+	self.frame_advances = 0
+	self.ended = false
+
+	local anim = self.anim
+	if not self.anim then return end
+
+	local durations = anim.duration
+	if self.frame_i > #durations then
+		self.frame_i = 1
+		self.ended = true
+	end
+
+	self.frame_t += dt
+	local duration = durations[self.frame_i] or durations[1]
+	while self.frame_t > duration do
+		self.frame_t -= duration
+
+		self.frame_i += 1
+		self.frame_advances += 1
+		if self.frame_i > #durations then
+			self.frame_i = 1
+			self.ended = true
+		end
+
+		duration = durations[self.frame_i]
+	end
+end
+
+--- Fetches the data in the current frame of the current animation for the given key.
+--- If there is data at index -1 of the array at that key, that is returned regardless of the current frame index.
+--- @param self Animator The animator.
+--- @param key any The key to index the animation by.
+--- @return any value The value fetched by the key.
+local function fetch(self,key)
+	local anim = self.anim
+	if not anim then return nil end
+	
+	local arr = anim[key]
+	if not arr then return nil end
+
+	return arr[self.frame_i] or arr[-1]
+end
+
+--- Resets the animator to a specific frame.
+--- @param self Animator The animator.
+--- @param frame number? The frame to reset to. Defaults to 1.
+local function reset(self,frame)
+	self.frame_i = frame or 1
+	self.frame_t = 0
+	self.frame_advances = 0
+	self.ended = false
+end
+
+--- @class Animator Responsible for handling animation playback state and frame data access.
+--- @field anim Animation The current animation, which indicates the timing and indexing.
+--- @field frame_i integer The index of the current frame.
+--- @field frame_t number The time that has elapsed since entering the current frame in seconds.
+--- @field frame_advances integer How many times the frame index has incremented during the last call to `advance`.
+--- @field ended boolean Whether or not the last call to `advance` advanced past the end of the animation.
+--- @field [any] any Any value which is present in the current animation and frame, with a key that doesn't match any of Animator's fields or methods.
+local m_animator = {
+	advance = advance,
+	reset = reset,
+}
+m_animator.__index = function(self,key)
+	return m_animator[key] or (key ~= "anim" and fetch(self,key))
+end
+
+--- Creates a new animator.
+--- @param anim Animation? The animation to initialize this animator with.
+--- @return Animator animator The newly created animator.
+local function new_animator(anim)
+	return setmetatable({
+		anim = anim,
+		frame_i = 1,
+		frame_t = 0,
+		frame_advances = 0,
+		ended = false,
+	},m_animator)
+end
+
+--- Generates an animation by a rule function.
+--- @param length integer The number of frames to add to the animation.
+--- @param rule function A function which takes in a frame index and returns a table with all the keys and values for that frame. The `duration` key is always required.
+--- @return Animation animation An Animation generated via the provided rule.
+local function animation_by_rule(length, rule)
+	local animation = {}
+	for frame_i = 1, length do
+		for k,v in pairs(rule(frame_i)) do
+			animation[k] = animation[k] or {}
+			animation[k][frame_i] = v
+		end
+	end
+
+	return animation
+end
+
+return {
+	new_animator = new_animator,
+	animation_by_rule = animation_by_rule,
+}
--- src/game.lua
+++ src/game.lua
@@ -1,23 +1,34 @@
 include"src/require.lua"
 
 -- Dependencies
+Animator = require"src/animation"
 
 -- Constants
 DT = 1/60
+local ANIM_IDLE <const> = Animator.animation_by_rule(
+	12,
+	function(frame_i) return {
+			duration = 0.2,
+			sprite = frame_i-1,
+	} end
+)
 
 -- Game state
+local animator --- @class Animator
 
 -- Picotron hooks
 function _init()
-
+	poke4(0x5000, fetch(DATP.."pal/0.pal"):get())
+	animator = Animator.new_animator(ANIM_IDLE)
 end
 
 function _update()
-	
+	animator:advance(DT)
 end
 
 function _draw()
-	
+	cls()
+	spr(animator.sprite)
 end
 
 include"src/error_explorer.lua"
