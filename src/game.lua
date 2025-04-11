4fd775f27f4986775ce71a2a75dc52cfe5297a1d
Drew "abledbody" Mudge <abledbody.gamedev@gmail.com>
2025-04-11 19:54:14 -0400

    Added screens

--- src/game.lua
+++ src/game.lua
@@ -1,19 +1,23 @@
 include"src/require.lua"
 
 -- Dependencies
-Animator = require"src/animation"
+local animation = require"src/animation"
+local screens = require"src/screens"
 
 -- Constants
 DT = 1/60
 local ANIMATIONS <const> = fetch(DATP.."anm/0.anm")
+local DRAW_CPU <const> = true
 
 -- Game state
 local animator --- @class Animator
+local screen_key --- @type string
 
 -- Picotron hooks
 function _init()
 	poke4(0x5000, fetch(DATP.."pal/0.pal"):get())
-	animator = Animator.new_animator(ANIMATIONS.idle)
+	animator = animation.new_animator(ANIMATIONS.idle)
+	screen_key = "start"
 end
 
 function _update()
@@ -22,7 +26,15 @@ end
 
 function _draw()
 	cls()
-	spr(animator.sprite)
+
+	local screen = screens.screens[screen_key]
+	screen:draw_bg()
+	spr(animator.sprite,52,139)
+	screen:draw_fg()
+
+	if DRAW_CPU then
+		print(string.format("CPU: %.2f%%",stat(1)*100),0,0,37)
+	end
 end
 
 include"src/error_explorer.lua"
---
+++ src/screen_data.lua
@@ -0,0 +1,26 @@
+--- @type table<string,ScreenData>
+return {
+	start = {
+		bg = {
+			{
+				sprite = 65,
+				pos = vec(30,61),
+			}
+		},
+		fg = {
+			{
+				sprite = 66,
+				pos = vec(31,93),
+			}
+		},
+		path = {
+			nodes = {
+				vec(88,206),
+				vec(410,206),
+			},
+			edges = {
+				userdata("i32",2,"0000000100000002"),
+			}
+		}
+	}
+}
